c2b38af78e3d0b9728e40942a028b5b2111011e5
James Benedict <jamesbenedict900@gmail.com>
2022-09-08 18:12:05 -0700

    task62

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	int a;
 	long int b;
 	long long int c;
-        char d;
+	char d;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
@@ -18,5 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-        return (0);
+return (0);
 }
