3993411fc368a61485c55ae5b2e5972fc63f0bfd
James Benedict <jamesbenedict900@gmail.com>
2022-09-10 07:49:07 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing lowercase alphabet
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+
+ 
