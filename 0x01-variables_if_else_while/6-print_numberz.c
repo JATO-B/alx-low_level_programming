64a3e86adadd65dd39444389d54df62f07c21134
James Benedict <jamesbenedict900@gmail.com>
2022-09-10 09:24:57 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing numbers in base ten
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int k;
+for (k = '0'; k <= '9'; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
