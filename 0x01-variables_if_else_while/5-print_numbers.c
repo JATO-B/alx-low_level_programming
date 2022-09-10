3b718acd68e9a99fd358141dad963161c77a4a5a
James Benedict <jamesbenedict900@gmail.com>
2022-09-10 09:07:39 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing all single digit numbers of base ten
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int b;
+for (b = 0; b <= 9; b++)
+{
+printf("%d", b);
+}
+printf("\n");
+return (0);
+}
