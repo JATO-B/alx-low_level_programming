fdbe217d8648f592901fc9a3d8c199d100ad9672
James Benedict <jamesbenedict900@gmail.com>
2022-09-10 09:41:31 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all base16 numbers
+ *
+ * Return: o
+ */
+
+int main(void)
+{
+char a;
+int b;
+for (b = '0'; b <='9'; b++)
+{
+putchar(b);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
