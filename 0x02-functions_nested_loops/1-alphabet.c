e953fc85f4708e61bf0838faeacb12a886e2d927
James Benedict <jamesbenedict900@gmail.com>
2022-09-14 10:33:31 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - entry point
-*
-* print_alphabet -  prints the alphabet of in lowercase
+* print_alphabet -  prints the alphabet, in lowercase, followed by a new line
 */
 
 void print_alphabet(void)
