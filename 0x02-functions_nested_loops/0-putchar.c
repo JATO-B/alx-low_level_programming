16d8ca9cb94e4f4ff7d6813198f380126eaee814
James Benedict <jamesbenedict900@gmail.com>
2022-09-14 04:54:03 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"                  
+/**                                                                          
+* main - printing _putchar    
+*
+* Return: always 0 success
+*/
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
+                                                                                                                                
+
+    
